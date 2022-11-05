27730d80b4fd5ed340187fe84ac8d416330833db
aimannasir12 <90481652+aimannasir12@users.noreply.github.com>
2022-11-06 02:23:09 +0530

    Update 4_👓_Financial Expert Sentiments.py

--- pages/4_👓_Financial Expert Sentiments.py
+++ pages/4_👓_Financial Expert Sentiments.py
@@ -44,7 +44,27 @@ if clicked1:
     if len(dropdown) > 0:
         news_tables = {}
         for ticker in dropdown:
-            parsed_data=pd.read_csv("investment_assistance/pages/df.csv")
+            url = finviz_url + ticker
+            req = Request(url=url, headers={'user-agent': 'my-app'})
+            response = urlopen(req)
+            html = BeautifulSoup(response, features='html.parser')
+            news_table = html.find(id='news-table')
+            news_tables[ticker] = news_table
+        parsed_data = []
+        for ticker, news_table in news_tables.items():
+
+            for row in news_table.findAll('tr'):
+
+                title = row.a.text
+                date_data = row.td.text.split(' ')
+
+                if len(date_data) == 1:
+                    time = date_data[0]
+                else:
+                    date = date_data[0]
+                    time = date_data[1]
+
+                parsed_data.append([ticker, date, time, title])
         df = pd.DataFrame(parsed_data, columns=['ticker', 'date', 'time', 'title'])
         nltk.download('vader_lexicon')
         vader = SentimentIntensityAnalyzer()
